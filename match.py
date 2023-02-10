7fcfa6c02300b8137752e95ec9ef8f39d592c27c
kaanlus <lusk@rpi.edu>
2023-02-10 16:52:00 -0500

    Tree for majors

---
+++ match.py
@@ -0,0 +1,56 @@
+
+# Make a dictionary of majors as keys, with the values being fields within that major
+
+# The next dictionary would use fields as keys, subfields as values
+
+# Keep getting smaller and more specific
+
+majors = {
+    "Computer Science": {
+        "Software Engineering":,
+        "Cybersecurity":,
+        "Data Science":,
+    },
+    "Mechanical Engineering": {
+        "Design":,
+        "Manufacturing":,
+        "Mechanics":,
+        "Thermodynamics":,
+        "Materials":
+    },
+    "Chemical Engineering": {
+        "Environmental":,
+        "Design":,
+        "Safety":,
+        "Plant":,
+        "Waste":
+    },
+    "Aerospace Engineering": {
+        "Aircraft":,
+        "Aerodynamics":,
+        "Thermodynamics":,
+        "Materials":,
+        "Mechanics": {
+            "Celestial":,
+            "Flight":,
+        }
+
+    },
+    "Electrical Engineering": {
+        
+    }
+
+
+}
+
+
+
+# Make some example functions that take in familiar keywords for different 
+
+# 
+
+# 
+
+# 
+
+# 
