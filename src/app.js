0f0ca0da2bdb5d0c038be8807f029dcb58532add
antmcgrann <antmcgrann@gmail.com>
2023-03-03 11:38:16 -0500

    Added submittable text fields

--- src/app.js
+++ src/app.js
@@ -10,3 +10,40 @@ switcher.addEventListener('click', function() {
         this.textContent = "Light";
     }
 });
+
+let userDataForm = document.getElementById("userInfo");
+userDataForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    let firstName = document.getElementById("fname");
+    let lastName = document.getElementById("lname");
+    let major = document.getElementById("major");
+    let resume = document.getElementById("resume");
+    let jsonData = document.getElementById("jsontext");
+
+    if (firstName.value == "" || lastName.value == "" || major.value == "" || resume.value == "") {
+        alert("MISSING REQUIRED FIELDS");
+    }
+    else {
+        alert("This form has been successfully submitted");
+        console.log(
+            "User: " + firstName.value + " " + lastName.value + "\n" +
+            "Major: " + major.value + "\n" +
+            "Resume Text: \n" +  resume.value);
+    }
+
+    // const fs = require('fs');
+    // let filename = firstName.value + "_" + lastName.value + "_" + major.value + ".txt";
+    // fs.writeFile(filename, resume.value, (err) => {
+    //     if (err) throw err;
+    //     else {
+    //         console.log("File successfully saved");
+    //     }
+    // });
+    
+
+    firstName.value = "";
+    lastName.value = "";
+    major.value = "";
+    resume.value = "";
+});
