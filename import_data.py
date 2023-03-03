1ad631de437d63470c37f691f6ba81a4dbe47ef0
rayzr522 <bsf003@yahoo.com>
2023-03-03 16:17:41 -0500

    Logic for cleanup of existing resources

--- import_data.py
+++ import_data.py
@@ -4,105 +4,95 @@ import time
 
 
 # Create boto3 clients
+# Verify flag set to false to silence http request cert warnings
 personalize_runtime = boto3.client('personalize-runtime', verify=False)
 personalize = boto3.client('personalize', verify=False)
 
 # Get Personalize Recipes
 response = personalize.list_recipes()
-
 for recipe in response['recipes']:
     print(recipe)
 
-# Set necessary ARN's & other variables
-datasetGroup= ''
+# Create schema
+schema = {
+    "type": "record",
+    "name": "Interactions",
+    "namespace": "com.amazonaws.personalize.schema",
+    "fields": [
+        {
+            "name": "USER_ID",
+            "type": "string"
+        },
+        {
+            "name": "ITEM_ID",
+            "type": "string"
+        },
+        {
+            "name": "TIMESTAMP",
+            "type": "long"
+        },
+        {
+            "name": "MAJOR",
+            "type": "string"
+        },
+        {
+            "name": "SKILLS",
+            "type": "string"
+        }
+    ],
+    "version": "1.0"
+}
+
+# Set necessary ARNs & other variables
+dataset_group = ''
 dataset = ''
-schemaInit = ''
+schema_arn = ''
 campaign = ''
 solution = ''
 role = ''
-dataSource = ''
+data_source = ''
 recipe = ''
 
-# Create schema
-schema = {
-  "type": "record",
-  "name": "Interactions",
-  "namespace": "com.amazonaws.personalize.schema",
-  "fields": [
-    {
-      "name": "USER_ID",
-      "type": "string"
-    },
-    {
-      "name": "ITEM_ID",
-      "type": "string"
-    },
-    {
-      "name": "TIMESTAMP",
-      "type": "long"
-    },
-    {
-      "name": "MAJOR",
-      "type": "string"
-    },
-    {
-      "name": "SKILLS",
-      "type": "string"
-    }
-  ],
-  "version": "1.0"
-}
-
 # Cleanups to remove old active resources that may interfere
+if len(personalize.list_datasets(datasetGroupArn=dataset_group, maxResults=50)['datasets']) != 0:
+    # Cleanup old dataset
+    cleanup_interactions_dataset = personalize.delete_dataset(dataset_arn=dataset)
 
+if len(personalize.list_campaigns(maxResults=50)['campaigns']) != 0:
+    # Cleanup old campaigns
+    cleanup_campaigns = personalize.delete_campaign(campaign_arn=campaign)
 
+if len(personalize.list_solutions(maxResults=50)['solutions']) != 0:
+    # Cleanup old solutions
+    cleanup_solutions = personalize.delete_solution(solution_arn=solution)
 
+if len(personalize.list_dataset_groups(maxResults=50)['datasetGroups']) != 0:
+    # Cleanup old dataset group
+    cleanup_interactions_dataset_group = personalize.delete_dataset_group(dataset_group_arn=dataset_group)
 
+if len(personalize.list_schemas(maxResults=50)['schemas']) != 0:
+    # Cleanup old schema
+    cleanup_interactions_schema = personalize.delete_schema(schema_arn=schema_arn)
 
 # Create new schema
-
-
-
 
 
 # Create Dataset Group
 
 
-
-
-
 # Create dataset in dataset group
-
-
-
 
 
 # Import data with a dataset import job
 
 
-
-
-
 # Create a solution
-
-
-
 
 
 # Create a solution version
 
 
-
-
-
 # Create a campaign from solution
 
 
-
-
-
 # Get recommendations from campaign
-
-
-
-
