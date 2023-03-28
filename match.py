de0e77a1117be0b51a2522ca01cd92c1ce0e71c8
kaanlus <lusk@rpi.edu>
2023-03-28 17:06:51 -0400

    function to make excel file

--- match.py
+++ match.py
@@ -1,9 +1,11 @@
 import json
+from openpyxl import Workbook
 # Make a dictionary of majors as keys, with the values being fields within that major
 
 # The next dictionary would use fields as keys, subfields as values
 
 # Keep getting smaller and more specific
+
 
 
 majors = {
@@ -46,10 +48,21 @@ majors = {
 
 
 }
-
+    
 
 
 # Make some example functions that take in familiar keywords for different 
+
+def make_workbook(path, jobs):
+    workbook = Workbook()
+    sheet = workbook.active
+    count = 0
+    for i in jobs:
+        count += 1
+        sheet['A' + str(count)] = i
+
+    workbook.save(path)
+    return workbook 
 
 def match(major):
     key = major.lower()
@@ -75,18 +88,34 @@ def spec(major):
 f = open("resume.json")
 data = json.load(f)
 
-i = data["students"]: 
+i = data["students"]
 print(i["major"])
 print(i["role"])
 
+name = str(data["name"])
 #Check if major is in our dict
 matches = match(i["major"])
 #If in dict, look for key words, else we add new key words to our new dictionary that this will create
+
+#Make empty array of jobs
+jobs = []
+
 if(matches):
     #start searching for key words
-    for x in majors[i["major"]]:
+    for x in data:
+        if (x in majors[i["major"]]):
+            #add to an array the jobs saved under the key
+            for job in majors[i["major"]]:
+                jobs.append(job)
+
+#Make file 
+
+job_file = make_workbook(name + "_jobs.xlsx", jobs)
+
+
         
 
 
 #return some sort of doc (maybe excel) with links to jobs in order of best match to worst (focus on ordering later)
- f.close()
+
+f.close()
